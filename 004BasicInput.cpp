8f44b3d4e1747acadd2ecb1695b6399973083a2f
johnmark2122 <johnmfajardo40gmail.com>
2024-10-07 09:29:54 +0800

    activity

---
+++ 004BasicInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	string nickname, course_year_level,school;
+	cout << "Nickname: ";
+	cin >> nickname;
+	cout << "Course-year level: ";
+	cin >> course_year_level;
+	cout << "school: ";
+	cin >> school;
+	return 0;
+}
+
+
+	
